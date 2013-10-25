7ed69482251416168c0c01d3e41a6a176787429f
Stephane Alnet <stephane@shimaore.net>
2013-10-25 16:19:44 +0200

    version bump

--- dist/moonshine.js
+++ dist/moonshine.js
@@ -1,4 +1,5 @@
-;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+!function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.moonshine=e():"undefined"!=typeof global?global.moonshine=e():"undefined"!=typeof self&&(self.moonshine=e())}(function(){var define,module,exports;
+return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var ccss, extend;
 
 extend = function(object, properties) {
@@ -615,4 +616,6 @@ if (typeof module !== "undefined" && module !== null) {
 
 
 },{"./ccss.coffee.md":1,"./coffeecup.js":2}]},{},[3])
+(3)
+});
 ;
